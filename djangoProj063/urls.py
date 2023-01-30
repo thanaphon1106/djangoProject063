3b146a1ec0411023d576702a10f0845b5f8e64ff
thanaphon1106 <thanaphon.wi@rmiti.ac.th>
2023-01-30 14:09:51 +0700

    commit

--- djangoProj063/urls.py
+++ djangoProj063/urls.py
@@ -19,7 +19,7 @@ from ProfileApp import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view.home, name='home'),
+    path('home',view.home, name='home'),
     path('personalRecord',view.personalRecord,name='personalRecord'),
     path('educationalRecord',view.educationalRecord,name='educationalRecord'),
     path('interests',view.interests,name='interests'),
